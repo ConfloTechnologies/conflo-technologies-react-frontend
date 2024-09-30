4c031385b4258d1007effbc1f02ee947e2748045
Morgan Cooper <morgan.cooper@suninenergy.com>
2024-09-29 21:04:51 -0700

    created dynamic content functionality for list views

--- src/common/utils/useDynamicContentHeightSettingOne.ts
+++ src/common/utils/useDynamicContentHeightSettingOne.ts
@@ -9,7 +9,7 @@ export const useDynamicContentHeight = (
         const headerHeight = headerRef.current ? headerRef.current.offsetHeight : 0;
         // Using the provided paginationHeight instead of a ref for now
         // const paginationHeight = paginationRef.current ? paginationRef.current.offsetHeight : 0; // cant get this working for some reason
-        const paginationHeight = 140;
+        const paginationHeight = 118;
         const totalOtherHeights = headerHeight + paginationHeight;
 
         console.log('Header Height:', headerHeight);
@@ -21,17 +21,22 @@ export const useDynamicContentHeight = (
 
     useLayoutEffect(() => {
         const resizeListener = () => updateContentHeight();
+        const orientationChangeListener = () => updateContentHeight();
+
         window.addEventListener('resize', resizeListener);
+        window.addEventListener('orientationchange', orientationChangeListener); // Listen for orientation changes
 
         // Initial run after mounting
         setTimeout(updateContentHeight, 100);
 
         return () => {
             window.removeEventListener('resize', resizeListener);
+            window.removeEventListener('orientationchange', orientationChangeListener);
         };
     }, [
         headerRef,
         // paginationHeight
         ]
-    ); // Depend on headerRef and paginationHeight
+    );
+
 };
