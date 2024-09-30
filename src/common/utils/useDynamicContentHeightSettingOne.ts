30a2825e17547036e39d9c0adf0166a36e0e761f
Morgan Cooper <morgan.cooper@suninenergy.com>
2024-09-29 21:10:50 -0700

    created dynamic content functionality for list views

--- src/common/utils/useDynamicContentHeightSettingOne.ts
+++ src/common/utils/useDynamicContentHeightSettingOne.ts
@@ -2,41 +2,42 @@ import { useLayoutEffect } from 'react';
 
 export const useDynamicContentHeight = (
     headerRef: React.RefObject<HTMLDivElement>,
-    // paginationRef: React.RefObject<HTMLDivElement>,
-    setMainContentHeight: (height: string) => void, // Default pagination height in case you can't get it dynamically
+    setMainContentHeight: (height: string) => void,
+    // paginationHeight: number = 116 // Default pagination height in case you can't get it dynamically
 ) => {
     const updateContentHeight = () => {
         const headerHeight = headerRef.current ? headerRef.current.offsetHeight : 0;
+
+        // Use window.innerHeight instead of 100vh to get the actual viewport height
         // Using the provided paginationHeight instead of a ref for now
         // const paginationHeight = paginationRef.current ? paginationRef.current.offsetHeight : 0; // cant get this working for some reason
-        const paginationHeight = 118;
+
+        const paginationHeight = 116;
+        const viewportHeight = window.innerHeight;
         const totalOtherHeights = headerHeight + paginationHeight;
 
+        console.log('Viewport Height:', viewportHeight);
         console.log('Header Height:', headerHeight);
         console.log('Pagination Height:', paginationHeight);
+
         console.log('Total Heights:', totalOtherHeights);
 
-        setMainContentHeight(`calc(100vh - ${totalOtherHeights}px)`);
+        // Subtract header and pagination heights from actual viewport height
+        setMainContentHeight(`${viewportHeight - totalOtherHeights}px`);
     };
 
     useLayoutEffect(() => {
         const resizeListener = () => updateContentHeight();
-        const orientationChangeListener = () => updateContentHeight();
-
         window.addEventListener('resize', resizeListener);
-        window.addEventListener('orientationchange', orientationChangeListener); // Listen for orientation changes
 
         // Initial run after mounting
         setTimeout(updateContentHeight, 100);
 
         return () => {
             window.removeEventListener('resize', resizeListener);
-            window.removeEventListener('orientationchange', orientationChangeListener);
         };
     }, [
         headerRef,
         // paginationHeight
-        ]
-    );
-
+    ]); // Depend on headerRef and paginationHeight
 };
